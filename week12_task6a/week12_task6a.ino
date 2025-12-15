0a431eaa7a24323dfc2b35bbe1c25ba0ef0f16aa
faskarovv <fasgerov06@gmail.com>
2025-12-16 02:50:10 +0400

    changes in comments

--- week12_task6a/week12_task6a.ino
+++ week12_task6a/week12_task6a.ino
@@ -6,14 +6,13 @@ unsigned long startTime;
 unsigned long endTime;
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+  Serial.begin(9600); //initalizing serial monitor
+  Wire.begin();  // Initializing as a  I2C master
   TWSR = 0;     // prescaler = 1
 
-  // TWBR = 255;  // ~31 kHz
+  // TWBR = 255;  // 30 ish kHz
   TWBR = 72;   // 200 kHz
   // TWBR = 12;    // 400 kHz
-  // ---------------------------
   delay(500);
 }
 
@@ -22,15 +21,15 @@ void loop() {
 
   for (int i = 0; i < 50; i++) {
 
-    // Send a byte
-    Wire.beginTransmission(slave_add);
+    // Send a byte to the slave 
+    Wire.beginTransmission(slave_add); 
     Wire.write(1);
     Wire.endTransmission();
 
     // Read reply
-    Wire.requestFrom(slave_add, 1);
-    while (!Wire.available());
-    uint8_t x = Wire.read();
+    Wire.requestFrom(slave_add, 1); //request 1 byte
+    while (!Wire.available()); //waiting until the byte is recived 
+    uint8_t x = Wire.read(); //reading byte
   }
 
   endTime = micros();
--- week12_task6b/week12_task6b.ino
+++ week12_task6b/week12_task6b.ino
@@ -1,20 +1,20 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(0x20);
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  Wire.begin(0x20); //setting the slaves adress
+  Wire.onReceive(receiveEvent); //this function is gonna be triggered when reciveing data
+  Wire.onRequest(requestEvent); // when master requests a data 
 }
 
-uint8_t lastByte = 0;
+uint8_t lastByte = 0; //storing the last recived data
 
 void receiveEvent(int howMany) {
   if (Wire.available())
-    lastByte = Wire.read();
+    lastByte = Wire.read(); //reading and saving 
 }
 
 void requestEvent() {
-  Wire.write(lastByte);
+  Wire.write(lastByte); //sending back the last recived data 
 }
 
 void loop() {}
