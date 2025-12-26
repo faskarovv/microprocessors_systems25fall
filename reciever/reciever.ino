8c7a4fafb27cc9f81eb243fbb6121359df8c66ea
faskarovv <fasgerov06@gmail.com>
2025-12-26 14:57:28 +0400

    changes in comments

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -9,6 +9,7 @@ uint8_t UART_receive() {
     return UDR0;
 }
 
+//simple if else to light the leds on
 void ledOn(uint8_t v) {
     PORTD &= ~((1<<PD2)|(1<<PD3)|(1<<PD4));
 
@@ -19,46 +20,46 @@ void ledOn(uint8_t v) {
 
 uint8_t read_eeprom_byte(uint8_t addr){
     while(EECR & (1 << EEPE));
-    EEAR = addr;
+    EEAR = addr; //set the address to read
     EECR |= (1<<EERE); //initalize read 
     return EEDR;
 }
 void eeprom_write(uint8_t addr, uint8_t data){
     while(EECR & (1 << EEPE));
 
-    EEAR = addr;
-    EEDR = data;
+    EEAR = addr; //addres to write to
+    EEDR = data; //data that is gonna be written
     uint8_t sreg = SREG;
     cli();
 
-    EECR |= (1 << EEMPE);
-    EECR |= (1 << EEPE);
+    EECR |= (1 << EEMPE); 
+    EECR |= (1 << EEPE); 
 
     SREG = sreg;
 }
 
 int main() {
     UBRR0H = 0;
-    UBRR0L = 103;
-    UCSR0B = (1 << RXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-
+    UBRR0L = 103; //baud rate 9600
+    UCSR0B = (1 << RXEN0); //enable reciever
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //8 bit data frame
+ 
     DDRD |= (1<<PD2)|(1<<PD3)|(1<<PD4); //setting as output for leds
 
     uint16_t addr = 0;
 
     while (1) {
-        uint8_t rx = UART_receive();
+        uint8_t rx = UART_receive(); 
 
         if (rx == '1' || rx == '2' || rx == '3') {
             if (addr < 1024) {
-                eeprom_write((uint8_t*)addr, rx - '0');
+                eeprom_write((uint8_t*)addr, rx - '0'); //writing to eeprom
                 addr++;
             }
         }
         else if (rx == '4') {
             for (uint16_t i = 0; i < addr; i++) {
-                uint8_t v = read_eeprom_byte((uint8_t*)i);
+                uint8_t v = read_eeprom_byte((uint8_t*)i); //when pressed 4 do the playback
                 ledOn(v);
                 _delay_ms(500);
             }
--- sender/sender.ino
+++ sender/sender.ino
@@ -5,15 +5,14 @@ uint8_t uart_read() {
 }
 
 void uart_write(uint8_t data) {
-    After a full byte is received, the hardware stores it in the UDR0 register and sets the RXC0 flag to indicate that new data is available. The software waits for this flag and reads UDR0 only when RXC0 is set, which clears the flag and allows the next byte to be received. For transmission, the hardware sets the UDRE0 flag when the data register is empty and ready for new data. The software waits for this flag and then writes a byte to UDR0. The hardware then automatically adds the start and stop bits and sends the data bit by bit on the TX line. When the register becomes empty again, UDRE0 is set, indicating that another byte can be transmitted.
-
-    UDR0 = data;
+    while (!(UCSR0A & (1 << UDRE0))); //waiting to see if the register is empty
+    UDR0 = data; //writing to the data register
 }
 
 void setup() {
 
     UBRR0H = 0;
-    UBRR0L = 103;                   // 9600 baud @ 16 MHz
+    UBRR0L = 103;                   // 9600 baud 16 MHz
     UCSR0B = (1 << RXEN0) | (1 << TXEN0);  // enable RX and TX
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8N1
 }
