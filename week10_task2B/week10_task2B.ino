2c20e85d34395ff4d4532247111938a6c96eff94
faskarovv <fasgerov06@gmail.com>
2025-12-26 15:15:53 +0400

    changes in comments

--- week10_task2B/week10_task2B.ino
+++ week10_task2B/week10_task2B.ino
@@ -3,16 +3,17 @@
 
 void UART_init() {
     UBRR0H = 0;
-    UBRR0L = 103;
-    UCSR0B = (1 << RXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    UBRR0L = 103; //baud rate 9600
+    UCSR0B = (1 << RXEN0); //enabling reciever
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //data frame 8 bit 
 }
 
 uint8_t UART_receive() {
-    while (!(UCSR0A & (1 << RXC0)));
-    return UDR0;
+    while (!(UCSR0A & (1 << RXC0))); //waiting for the status flag
+    return UDR0; //returning the data registers value
 }
 
+//simple if else to light the leds up 
 void showLED(uint8_t v) {
     PORTD &= ~((1<<PD2)|(1<<PD3)|(1<<PD4));
 
@@ -23,7 +24,7 @@ void showLED(uint8_t v) {
 
 int main() {
     UART_init();
-    DDRD |= (1<<PD2)|(1<<PD3)|(1<<PD4);
+    DDRD |= (1<<PD2)|(1<<PD3)|(1<<PD4); //sets as output
 
     while (1) {
         uint8_t rx = UART_receive();
